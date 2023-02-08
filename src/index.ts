32c2088ebb6edaa35fe32bf72235563d1e289ba9
Josia Scheerle <Josia.Scheerle@opendoors.de>
2023-02-08 19:35:15 +0100

    Write PseudoCode for the wanted function

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 var xlWrite = require('write-excel-file/node')
 
-
 const HEADER_ROW = [
     {
         value: 'Titel',
@@ -15,26 +14,43 @@ const HEADER_ROW = [
         fontWeight: 'bold'
     },
     {
-        value: 'Dauer',
+        value: 'Dauer in s',
         fontWeight: 'bold'
     }
 ]
+let clips:Array<Object>
 
 //title | Artist | Domain | Duration
-let title = 'Avalanche'
-let artist = 'Bring Me The Horizon'
-let domain = 'Spotify'
-let duration = '100s'
-
-const DATA_ROW = [ { value: title }, { value: artist },{ value: domain },{ value: duration }]
+let title:String
+let artist:String
+let domain:String
+let duration:Number
 
 let data:Array<Array<Object>> = [
     HEADER_ROW
 ]
+for (let i = 0; i < clips.length; i++ ) {
+    
+    let currentClip = clips[i];
 
-data.push(DATA_ROW)
-// const HEADER_ROW = ['Titel', 'Interpret', 'Domain', 'Dauer']
-// const DATA_ROW1 = ['Avalanche', 'Bring Me The Horizon', 'Spotify', '100s']
+    //! PSEUDO-CODE
+    /* if (clip[i].origin != "project-directory/**_Musik") {
+        return
+    }
+    title = currentClip.name;
+    artist = currentClip.artist;
+    domain = '';
+    duration = currentClip.outPoint - currentClip.inpoint;           //Format to seconds
+    if(data.exist(currentClip.name){
+        data[data.getIndexForClip(currentClip.name)].duration += duration;
+        return;
+    }
+    const DATA_ROW = [ { value: title }, { value: artist },{ value: domain },{ value: duration }];
+    
+    data.push(DATA_ROW);
+    */
+
+}
 
 xlWrite(data, {
     filePath: "test.xlsx"
